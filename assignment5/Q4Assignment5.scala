d34c161373e5185e2a139a417434c306fe5820bc
Sanduni2019 <56638280+Sanduni2019@users.noreply.github.com>
2020-07-31 00:20:01 +0530

    Add files via upload

--- assignment5/Q4Assignment5.scala
+++ assignment5/Q4Assignment5.scala
@@ -3,22 +3,26 @@ object Q4Assignment5 extends App{
 	//list of 5 accounts are initialized
 	val bank:List[Account]=List(new Account("972540808V" , 1001 ,25000.00),new Account("91234508V" , 1002 ,-20000.50),new Account("236540808V" , 1003 ,15000.00),new Account("258960808V" , 1004 ,10000.50),new Account("995123808V" , 1005 ,5500.00))
 	
-	println("bank Details : ")
+	println("\n\nInitial Bank Details : \n\t")
 	println(bank)
 	
 	val overdraft = (a:List[Account])=>a.filter(x=>x.balance<0)
-	println("Overdraft : \n\n"+overdraft(bank))
+	println("\n\nOverdraft : \n\t"+overdraft(bank))
 	
+	
+	val balance = (a:List[Account])=>a.map(x=>x.balance).reduce((x,y)=>x+y) // first map the account balances and then get the total
+	println("\n\nTotal of Accounts : \n\t"+balance(bank))
 	
 	val interest = (a:List[Account])=>a.map( x=> 
 		if(x.balance>0){ new Account(x.nic,x.accNum,x.balance*1.05)} 
-		else { new Account(x.nic,x.accNum,x.balance*1.10) }
+		else { new Account(x.nic,x.accNum,x.balance*1.10) })
+	
+	val bankNew = interest(bank)
 		
-	println("Account Details after Interest Calculation : \n\n"+interest(bank)))
+	println("\n\nAccount Details after Interest Calculation : \n\t"+bankNew)
 	
-	
-	val balance = (a:List[Account])=>a.map(x=>x.balance).reduce((x,y)=>x+y)
-	println("Total of Accounts : \n\n"+balance(bank))
+	println("\n\nUpdated Total of Accounts : \n\t"+balance(bankNew))
+
 
 }
 
